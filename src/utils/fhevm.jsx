b8065f9407275b6a5ee551ef76db1b9295b8ba3d
Amaury <1293565+amaury1729@users.noreply.github.com>
2024-02-12 11:27:03 +0100

    chore: Update to Gentry Testnet

--- src/utils/fhevm.jsx
+++ src/utils/fhevm.jsx
@@ -1,8 +1,11 @@
-import { BrowserProvider, JsonRpcProvider } from "ethers";
+import { BrowserProvider, AbiCoder } from "ethers";
 import { initFhevm, createInstance } from "fhevmjs";
 export const init = async () => {
   await initFhevm();
 };
+
+// From https://github.com/zama-ai/fhevmjs/blob/c4b8a80a8783ef965973283362221e365a193b76/bin/fhevm.js#L9
+const FHE_LIB_ADDRESS = "0x000000000000000000000000000000000000005d";
 
 export const provider = new BrowserProvider(window.ethereum);
 
@@ -11,10 +14,14 @@ let instance;
 export const createFhevmInstance = async () => {
   const network = await provider.getNetwork();
   const chainId = +network.chainId.toString();
-  const publicKey = await provider.call({
-    from: null,
-    to: "0x0000000000000000000000000000000000000044",
+  // Get blockchain public key
+  const ret = await provider.call({
+    to: FHE_LIB_ADDRESS,
+    // first four bytes of keccak256('fhePubKey(bytes1)') + 1 byte for library
+    data: "0xd9d47bb001",
   });
+  const decoded = AbiCoder.defaultAbiCoder().decode(["bytes"], ret);
+  const publicKey = decoded[0];
   instance = await createInstance({ chainId, publicKey });
 };
 
